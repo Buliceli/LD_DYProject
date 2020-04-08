4212e30be3acd58b17c9e587de5bf2fdbaa0fd28
李洞洞 <buliceli@163.com>
2020-04-08 09:01:05 +0800

    初始化项目

---
+++ LD_DYProject/LD_DYProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LD_DYProject
+//
+//  Created by 李洞洞 on 2020/4/8.
+//  Copyright © 2020 李洞洞. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ LD_DYProject/LD_DYProjectTests/LD_DYProjectTests.swift
@@ -0,0 +1,34 @@
+//
+//  LD_DYProjectTests.swift
+//  LD_DYProjectTests
+//
+//  Created by 李洞洞 on 2020/4/8.
+//  Copyright © 2020 李洞洞. All rights reserved.
+//
+
+import XCTest
+@testable import LD_DYProject
+
+class LD_DYProjectTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
